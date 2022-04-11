f7f89259cc242103c49425960e1548e9e5690225
Kim <deviluk333@gmail.com>
2022-04-11 12:56:53 +0300

    v3
    - still one pair RUB/USD
    - changed highcharts Jquery to Charts.js
    - added ability to plot previous points on chart

--- graph.py
+++ graph.py
@@ -5,6 +5,7 @@ from random import random
 from datetime import datetime
 import psycopg2
 
+#Init FLask application
 app = Flask(__name__)
 
 #Establishing the connection
@@ -15,29 +16,33 @@ conn = psycopg2.connect(
     host='127.0.0.1', 
     port= '5432')
 
+#Init cursor for db
 cursor = conn.cursor()
 
 #Setting auto commit false
 conn.autocommit = True
 
-def generate_movement():
-    movement = -1 if random() < 0.5 else 1
-    return movement
-
-value = [[], [], 'name']
-
-@app.route('/', methods=["GET", "POST"])
-def main():
-    return render_template('index.html')
-
+#Getting data from db
 cursor.execute('''SELECT * from currency''')
 result = cursor.fetchall();
 rub_usd_price = float(result[-1][0])
 tme = float(result[-1][1])
 
+#const for points
+value = [[], [], 'RUB/USD']
+
+#Price movoment initation
+def generate_movement():
+    movement = -1 if random() < 0.5 else 1
+    return movement
+
+#Core page
+@app.route('/', methods=["GET", "POST"])
+def main():
+    return render_template('index.html')
+
 @app.route('/init', methods=["GET"])
 def init():
-
     response = make_response(json.dumps(value))
     response.content_type = 'application/json'
     return response
@@ -49,19 +54,14 @@ def data():
     rub_usd_price = rub_usd_price + generate_movement()
     if rub_usd_price <=0:
         rub_usd_price = 0
-    
     tme = tme + 1000
     value[0].append(tme)
     value[1].append(rub_usd_price)
-    a = [tme, rub_usd_price]
-    
- #  data = [tme , rub_usd_price]
-
-
+    values = [tme, rub_usd_price]
+    # data = [tme , rub_usd_price]
     # cursor.execute('''INSERT INTO currency (price, time) VALUES (%s, %s)''', [rub_usd_price, tme])
     # conn.commit()
-
-    response = make_response(json.dumps(a))
+    response = make_response(json.dumps(values))
     response.content_type = 'application/json'
     return response
 
